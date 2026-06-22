3768d7d9da7aef28f7accb3eb6c62733420cf500
Mohd Safwan <safanr226s@gmail.com>
2026-06-22 20:48:30 +0530

    Initial commit

---
+++ vercel/src/assets/banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      
+      <div className="buttons">
+        <button className="1">My Society</button>
+        <button className="2">Contact Us</button>
+        <button className="3">Free Subscrition</button>
+      </div>
+    </div>
+  )
+}
+
+export default banner;
---
+++ vercel/src/assets/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="logo"></div>
+    </div>
+  )
+}
+
+export default Navbar
